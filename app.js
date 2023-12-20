fa9f9ee5c2d0c56891cd818a4b549f6f7861fead
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-20 08:23:31 -0600

    commit-1

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+let data = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut"
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "qui est esse",
+        "body": "est rerum tempore"
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "ea molestias quasi"
+    }
+];
+
+//express middleware 
+app.use('/posts', (req, res) => {
+    // res.json(200, data[1]);
+    res.status(200).json(data);
+});
+app.use('/', (req, res)=>{
+    res.status(200).send('I did it :) ')
+});
+
+
+app.listen(5000, ()=>{
+    console.log('Your Server is running on port 5000...')
+});
